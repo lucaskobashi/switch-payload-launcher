387470904eedd2afca83f869a3280f210fecf4ac
lucas kobashi <lucas.kobashi@cs.usask.ca>
2022-06-23 19:27:21 -0600

    Initial Commit

---
+++ Switch Payload Launcher/Switch_Payload_LauncherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Switch_Payload_LauncherApp.swift
+//  Switch Payload Launcher
+//
+//  Created by Lucas Kobashi on 23-06-2022.
+//
+
+import SwiftUI
+
+@main
+struct Switch_Payload_LauncherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
